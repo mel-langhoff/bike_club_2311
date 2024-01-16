cd90afc30a2a43af1adba45daf12c93e2c1e4606
Mel Langhoff <mlanghoff@uwalumni.com>
2024-01-16 09:21:26 -0700

    Add tests for adding rides

--- spec/biker_spec.rb
+++ spec/biker_spec.rb
@@ -4,6 +4,9 @@ require "./lib/biker"
 RSpec.describe Biker do
     before :each do
         @biker = Biker.new("Kenny", 30)
+        @ride1 = Ride.new({name: "Walnut Creek Trail", distance: 10.7, loop: false, terrain: :hills})
+        @ride2 = Ride.new({name: "Town Lake", distance: 14.9, loop: true, terrain: :gravel})
+        @biker2 = Biker.new("Athena", 15)
     end
 
     it "exists" do
@@ -18,6 +21,19 @@ RSpec.describe Biker do
 
         expect(@biker.acceptable_terrain).to eq([:gravel, :hills])
     end
+
+    it "#log_ride" do
+        expect(@biker.rides).to eq([])
+
+        @biker.log_ride(@ride1, 92.5)
+        @biker.log_ride(@ride1, 91.1)
+        @biker.log_ride(@ride2, 60.9)
+        @biker.log_ride(@ride2, 61.6)
+
+        expect(@biker.rides).to include([92.5, 91.1])
+    end
+
+
 
 
 end
